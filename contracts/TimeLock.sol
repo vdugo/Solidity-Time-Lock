4731b71187905bcf63027ce5186a11ad56e8c012
Vincent <v3rdugo@yahoo.com>
2022-08-24 14:51:44 -0700

    set the owner in constructor and set up queue function parameters

--- contracts/TimeLock.sol
+++ contracts/TimeLock.sol
@@ -3,5 +3,21 @@ pragma solidity ^0.8.10;
 
 contract TimeLock
 {
-    
+    address public owner;
+
+    constructor()
+    {
+        owner = msg.sender;
+    }
+
+    function queue(
+        address _target,
+        uint256 _value,
+        string calldata _func,
+        bytes calldata _data,
+        uint256 _timestamp
+    ) external
+    {
+        
+    }
 }
