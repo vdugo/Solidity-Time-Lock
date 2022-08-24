4f604fc893e2cb4b444483bc1b53aa46d250749e
Vincent <v3rdugo@yahoo.com>
2022-08-24 15:20:12 -0700

    complete queueing the transaction

--- contracts/TimeLock.sol
+++ contracts/TimeLock.sol
@@ -4,8 +4,24 @@ pragma solidity ^0.8.10;
 contract TimeLock
 {
     error NotOwnerError();
+    error AlreadyQueuedError(bytes32 txId);
+    error TimestampNotInRangeError(uint256 blockTimestamp, uint256 timestamp);
+
+    event Queue(
+        bytes32 indexed txId,
+        address indexed target,
+        uint256 value,
+        string func,
+        bytes data,
+        uint256 timestamp
+    );
+
+    uint256 public constant MIN_DELAY = 10 seconds;
+    uint256 public constant MAX_DELAY = 1000 seconds;
+
 
     address public owner;
+    mapping(bytes32 => bool) queued;
 
     constructor()
     {
@@ -45,5 +61,28 @@ contract TimeLock
     ) external onlyOwner
     {
         bytes32 txId = getTxId(_target, _value, _func, _data, _timestamp);
+        if (queued[txId])
+        {
+            revert AlreadyQueuedError(txId);
+        }
+
+        if (
+            _timestamp < block.timestamp + MIN_DELAY ||
+            _timestamp > block.timestamp + MAX_DELAY
+        )
+        {
+            revert TimestampNotInRangeError(block.timestamp, _timestamp);
+        }
+
+        queued[txId] = true;
+        
+        emit Queue(
+            txId,
+            _target,
+            _value,
+            _func,
+            _data,
+            _timestamp
+        );
     }
 }
